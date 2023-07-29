361ebc67d65946d5c00ead1e651328460f650b67
maiAbdulhameed <mai.abdelhameed@rowaad.net>
2023-07-29 05:50:11 +0300

    Project 1 starting files

---
+++ projects/01-project/starting-project/src/App.js
@@ -0,0 +1,30 @@
+import "./index.css";
+const questions = [
+  {
+    id: 1,
+    title: "Is this a good product?",
+    info:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem."
+  },
+  {
+    id: 2,
+    title: "How much does it cost?",
+    info:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem."
+  },
+  {
+    id: 3,
+    title: "When can I get it?",
+    info:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere in labore maxime, assumenda iure sed tenetur alias omnis eveniet similique laborum, neque porro unde ducimus officiis animi vitae! Quidem."
+  }
+];
+function App() {
+  return (
+    <div className="app">
+      <h1>Project: FAQ/Accordion</h1>
+    </div>
+  );
+}
+
+export default App;
